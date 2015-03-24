97e451825a4ff5e76ea144d88a28b581a7ec97b0
Mahieddine Yaker <mahieddine.yaker@gmail.com>
2015-03-24 17:43:21 +0100

    avancement sur le tp avec ajout de quelques methodes

--- src/Server.java
+++ src/Server.java
@@ -1,13 +1,23 @@
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 
+/**
+ * @author mahieddine
+ *
+ */
 public class Server {
 
 	public static void main(String[] args) {
 		
-		SiteImpl skeleteon = (SiteImpl) UnicastRemoteObject.exportObject(new SiteImpl());
+		try {
+			SiteItf skeleteon = (SiteItf) UnicastRemoteObject.exportObject(new SiteImpl());
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/SiteImpl.java
+++ src/SiteImpl.java
@@ -1,22 +1,60 @@
 import java.rmi.RemoteException;
-import java.rmi.registry.Registry;
+import java.util.ArrayList;
 
 
 public class SiteImpl implements SiteItf {
 
+	private ArrayList<SiteItf> listeFils;
+	private ArrayList<SiteItf> listePere;
 	
+	/**
+	 * 
+	 */
 	public SiteImpl(){
-		
 	}
+
+	
+	
+	/* (non-Javadoc)
+	 * @see SiteItf#getMessage()
+	 */
 	public void getMessage() throws RemoteException{
-		// TODO Auto-generated method stub
-		
+		System.out.println("message Reçu");
+		this.sendMessage();
 	}
 
 	
 	public void sendMessage() throws RemoteException{
-		// TODO Auto-generated method stub
-		
+		for (SiteItf siteItf : listeFils) {
+			siteItf.getMessage();
+		}
+	}
+	
+	public void init(){
+		new Thread(){
+			public void run(){
+				
+			}
+		}.start();
+	}
+
+
+
+
+	public boolean addPere(SiteItf pere) {
+
+		if(!listePere.contains(pere))
+			return listePere.add(pere);
+		return false;
+	}
+
+
+
+	@Override
+	public boolean addFils(SiteItf fils) {
+		if(!listeFils.contains(fils))
+			return listeFils.add(fils);
+		return false;
 	}
 
 }
